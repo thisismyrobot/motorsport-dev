67806675ce2f5e262d1232947eb63fd1c87ec9c4
Robert Wallhead <rwallhead@gmail.com>
2009-09-01 18:58:34 +1000

    Made it so that you can click to draw dots.

--- src/test.py
+++ src/test.py
@@ -1,55 +1,49 @@
 import pyglet
 import pyglet.window
 import pyglet.graphics
-       
-window = pyglet.window.Window()
-label = pyglet.text.Label('Hello, world', 
-                          font_name='Times New Roman', 
-                          font_size=12,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
 
-fps_display = pyglet.clock.ClockDisplay()
-
-dots = pyglet.graphics.vertex_list(2,
-    ('v2i', (10, 15, 30, 35)),
-    ('c3B', (255, 0, 0, 255, 0, 0))
-)
-
-def add_dot(x, y):
-    size = dots.get_size()
-    size = size + 1
-
-    v2i = dots.verticies
-    c3b = dots.colors
-
-    v2i.append(x)
-    v2i.append(y)
-    c3b.append(255)
-    c3b.append(0)
-    c3b.append(0)
-
-    dots = pyglet.graphics.vertex_list(size,
-        ('v2i', v2i),
-        ('c3B', c3b)
+class gps_map:
+    red_dots = pyglet.graphics.vertex_list(1,
+        ('v2i', (10, 15)),
+        ('c3B', (255, 0, 0))
     )
+    
+    def draw(self):
+        self.red_dots.draw(pyglet.gl.GL_POINTS)
+    
+    def add_red_dot(self, x, y):
+        """ Adds a dot to the list of red dots
+        """
+        size = self.red_dots.get_size()
+        size = size + 1
 
+        v2i = list(self.red_dots.vertices)
+        c3b = list(self.red_dots.colors)
+
+        v2i.append(x)
+        v2i.append(y)
+        c3b.append(255)
+        c3b.append(0)
+        c3b.append(0)
+
+        self.red_dots = pyglet.graphics.vertex_list(size,
+            ('v2i', v2i),
+            ('c3B', c3b)
+        )
+
+window = pyglet.window.Window()
+fps_display = pyglet.clock.ClockDisplay()
+map = gps_map()
 
 @window.event
 def on_draw():
     window.clear()
-    label.draw()
     fps_display.draw()
-    dots.draw(pyglet.gl.GL_POINTS)
-    #import pdb; pdb.set_trace()
-
+    map.draw()
+ 
 @window.event
 def on_mouse_press(x, y, button, modifiers):
     if button == pyglet.window.mouse.LEFT:
-        add_dot(x, y)
-        
-        #label.x = x
-        #label.y = y
-
+        map.add_red_dot(x, y)
 
 pyglet.app.run()
