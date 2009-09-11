e29b77e23e7fdaf2c87ce4ee3ef2b30aef61655a
Robert Wallhead <rwallhead@gmail.com>
2009-09-11 22:56:33 +1000

    Made the code a little more resilient to timing out/comms errors.

--- src/sandboxing/gsp_rs232_tester.py
+++ src/sandboxing/gsp_rs232_tester.py
@@ -6,9 +6,12 @@ conn = serial.Serial(port, 4800, timeout=2)
 
 while True:
     line = conn.readline().split(',')
-    if line[0] == "$GPRMC":
-        lat = line[3]
-        long = line[5]
-        lat_s = "-%d %.4f" % (int(lat[0:2]), float(lat[2:]))
-        long_s = "%d %.4f" % (int(long[0:3]), float(long[3:]))
-        print "%s %s" % (lat_s, long_s)
+    try:
+        if line[0] == "$GPRMC":
+            lat = line[3]
+            long = line[5]
+            lat_s = "-%d %.4f" % (int(lat[0:2]), float(lat[2:]))
+            long_s = "%d %.4f" % (int(long[0:3]), float(long[3:]))
+            print "%s %s" % (lat_s, long_s)
+    except:
+        print line
