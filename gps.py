d576664fd78570301282614a4fed006a9c49466f
Robert Wallhead <rwallhead@gmail.com>
2016-01-21 14:49:35 +1100

    New version that pulls in GPS data from all USB ports available

--- gps.py
+++ gps.py
@@ -1,15 +1,51 @@
-import led
-import serial
+import multiprocessing
+import serial.tools.list_ports
+import time
 
 
-ser = serial.Serial('/dev/ttyAMA0', 4800)#, timeout=1)
-led_disp = led.LED()
+def reader(queue, path):
+    """ Process to read from a comm port and put lines on a queue.
+    """
+    # Connect at 9600 baud
+    conn = serial.Serial(path, timeout=1)
 
-while True:
-    line = ser.readline()
-    if line.startswith('$GPVTG'):
-        kph = line.split(',')[7]
-        if kph == '':
-            led_disp.set('NO GPS')
-        else:
-            led_disp.set('{} kph'.format(kph))
+    # Push data on to the queue.
+    while True:
+        # Check we have a GPS device
+        line = conn.readline()
+        if not line.startswith('$GP'):
+            return
+        if line.startswith('$GPVTG'):
+            queue.put(':'.join((path, line)))
+
+
+def writer(queue):
+    """ Process to read from a queue.
+    """
+    while True:
+        print queue.get()
+
+
+def go():
+    """ Multiplex GPS data.
+    """
+    queue = multiprocessing.Queue()
+
+    multiprocessing.Process(target=writer, args=(queue,)).start()
+
+    readers = {}
+
+    while True:
+
+        for (path, _, _) in serial.tools.list_ports.comports():
+
+            if path not in readers.keys() or not readers[path].is_alive():
+
+                readers[path] = multiprocessing.Process(
+                    target=reader, args=(queue, path)
+                )
+                readers[path].start()
+
+
+if __name__ == '__main__':
+    go()
